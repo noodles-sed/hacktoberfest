eca216294139fc90e0a64bda7f300ff0b32415d8
Manav Gosain <k.gosain7140@gmail.com>
2024-10-18 23:20:49 +0530

    added solution to Question 14

---
+++ Q14.java
@@ -0,0 +1,25 @@
+// program to check leap year
+
+import java.util.Scanner;
+
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the year: ");
+        int year = sc.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    System.out.println(year + " is a leap year");
+                else
+                    System.out.println(year + " is not a leap year");
+            } else
+                System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+
+        sc.close();
+    }
+}
